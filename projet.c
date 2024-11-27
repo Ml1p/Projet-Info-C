117023d4ab5b875664e0b0f8ef8ba5e007ebe2e5
Ml1p <Poubelle97434@laposte.net>
2024-11-27 14:07:03 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <time.h>
 #include <unistd.h>
+#include<math.h>
 #define N 10
 #define EPS 1.e-8
 
@@ -48,7 +49,13 @@ int main(void) {
   struct jeu charge_partie(int);
 
   struct jeu p2;
-  p2=charge_partie(0);
+  init_jeu(p2);
+  for(int x=0;x<LARGEUR;x++)
+    for(int y=0;y<HAUTEUR;y++)
+      p2.grille[x][y]=3;
+  p2.score=-300;
+  p2.taille=34;
+  sauvegarde_partie(p2,0);
 
   int i=0;
   while(i<0) {
@@ -270,7 +277,73 @@ struct jeu charge_partie(){
 
 void sauvegarde_partie(struct jeu j, int numero_sauvegarde){
 
+  int taille_ligne=LARGEUR*HAUTEUR*2+7+2+3;  // Nombre de char de la grille + Nombre de char du score + Nb de char de la taille du radeau
+
   FILE *fichier_sauvegarde=fopen("fichier_sauvegarde.txt","r");
+
+  fseek(fichier_sauvegarde,0,SEEK_END); // Va à la fin du fichier pour connaitre le nombre de charactères dans le fichier
+  int nb_lignes_fichier=ftell(fichier_sauvegarde)/taille_ligne; // Nombre de Charactères / Nombre de charactères dans une ligne = Nombre de lignes
+  fseek(fichier_sauvegarde,0,SEEK_SET); // Retourne au début du fichier
+
+
+  // Stoppe la sauvegarde si le numéro de sauvegarde est trop grand
+
+  if(numero_sauvegarde>nb_lignes_fichier+1)
+    return;
+
+
+  // Enregistre l'entièreté du fichier
+
+  char contenu_fichier[nb_lignes_fichier][taille_ligne];
+
+  for(int i=0; i<nb_lignes_fichier;i++){
+    
+    fgets(contenu_fichier[i],taille_ligne,fichier_sauvegarde);
+    fseek(fichier_sauvegarde,0,SEEK_CUR);
+  }
+
+
+  // Modifie la ligne de la sauvegarde
+
+  if(numero_sauvegarde<=nb_lignes_fichier){
+
+    for(int y=0;y<HAUTEUR;y++)
+      for(int x=0;x<LARGEUR;x++){
+
+        contenu_fichier[numero_sauvegarde][x*2+y*LARGEUR*2]=j.grille[x][y]+'0'; // + '0' Convertis l'int en char
+        contenu_fichier[numero_sauvegarde][x*2+1+y*LARGEUR*2]='\t';
+      }
+
+    char buffer[6]="000000";
+
+    if(j.score<0){
+
+      buffer[5]='-';
+      j.score=-j.score;
+    }
+    for(int i=0;i<6;i++){
+
+      buffer[5-i]=j.score%(int)(1000000/pow(10,i))+'0';
+      printf("Puissance 10 : %d\tCoeff score : %d\tBuffer : %s\n",(int)(100000/pow(10,i)),(int)(j.score/(100000/pow(10,i))),buffer);
+    }
+    
+    printf("%s\n",buffer);
+
+
+    for(int i=0;i<6;i++){
+
+      contenu_fichier[numero_sauvegarde][HAUTEUR*LARGEUR*2+i]=buffer[i];
+    }
+
+    printf("%s\n",contenu_fichier[numero_sauvegarde]);
+
+    contenu_fichier[numero_sauvegarde][HAUTEUR*LARGEUR*2+7]='\t';
+
+
+
+  }
+
+  /*fichier_sauvegarde=fopen("fichier_sauvegarde.txt","r");
 
   // Enregistre la grille sur la première ligne
 
@@ -286,7 +359,7 @@ void sauvegarde_partie(struct jeu j, int numero_sauvegarde){
 
   fprintf(fichier_sauvegarde,"%d",j.taille);
 
-  fclose(fichier_sauvegarde);
+  fclose(fichier_sauvegarde);*/
 }
 
 struct jeu charge_partie(int numero_sauvegarde){
@@ -295,18 +368,17 @@ struct jeu charge_partie(int numero_sauvegarde){
   struct jeu j;
   int taille_ligne=LARGEUR*HAUTEUR*2+7+3;  // Nombre de char de la grille + Nombre de char du score + Nb de char de la taille du radeau
   char buffer[16];
-  
+
   // Se déplace dans le fichier à la ligne de la sauvegarde à charger
 
   fseek(fichier_sauvegarde,taille_ligne*numero_sauvegarde,SEEK_SET);
-  
+
   // Charge la grille
 
   for(int y=0;y<HAUTEUR;y++)
     for(int x=0;x<LARGEUR;x++){
       
       fgets(buffer,2,fichier_sauvegarde);
-      //printf("Char n° : %d\tValeur : %s\n",ftell(fichier_sauvegarde),buffer);
 
       sscanf(buffer,"%d",&j.grille[x][y]);
       fseek(fichier_sauvegarde,1,SEEK_CUR); // Décale la lecture sur la ligne
@@ -322,10 +394,6 @@ struct jeu charge_partie(int numero_sauvegarde){
   fseek(fichier_sauvegarde,1,SEEK_CUR);
   fgets(buffer,3,fichier_sauvegarde);
   sscanf(buffer,"%d",&j.taille);
-
-  fseek(fichier_sauvegarde,1,SEEK_CUR);
-  printf("%d - %d\t%s\n",taille_ligne,ftell(fichier_sauvegarde),buffer);
-  fgets(buffer, 16,fichier_sauvegarde);
 
   fclose(fichier_sauvegarde);
 
